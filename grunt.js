895feaa7a770cdd1342c1e019f0a0553d856438e
Loïc Frering <loic.frering@gmail.com>
2012-11-09 22:15:50 +0100

    Use Grunt for project management.

---
+++ grunt.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    lint: {
+      src:   'src/**/*.js',
+      grunt: 'grunt.js'
+    },
+    mocha: {
+      index: {
+        src: ['test/index.html']
+        //run: true
+      }
+    }
+  });
+
+  // Grunt plugins.@
+  grunt.loadNpmTasks('grunt-mocha');
+
+  // Default task.
+  grunt.registerTask('default', 'lint mocha');
+};
