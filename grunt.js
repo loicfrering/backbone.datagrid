6c0425fd52d32c6163531e1cb43c3cb6074c2cff
Loïc Frering <loic.frering@gmail.com>
2012-11-10 01:47:02 +0100

    Add concat and min grunt tasks for distribution.

--- grunt.js
+++ grunt.js
@@ -8,6 +8,23 @@ module.exports = function(grunt) {
     },
     mocha: {
       all: ['test/index.html']
+    },
+    concat: {
+      dist: {
+        src: [
+          'src/models/*.js',
+          'src/views/cell.js', 'src/views/callback-cell.js', 'src/views/action-cell.js', 'src/views/header-cell.js',
+          'src/views/header.js', 'src/views/row.js', 'src/views/pagination.js',
+          'src/views/datagrid.js'
+        ],
+        dest: 'dist/backbone.datagrid.js'
+      }
+    },
+    min: {
+      dist: {
+        src: 'dist/backbone.datagrid.js',
+        dest: 'dist/backbone.datagrid.min.js'
+      }
     }
   });
 
@@ -16,5 +33,6 @@ module.exports = function(grunt) {
 
   // Default task.
   grunt.registerTask('test', 'lint mocha');
-  grunt.registerTask('default', 'lint mocha');
+  grunt.registerTask('dist', 'concat min');
+  grunt.registerTask('default', 'lint mocha concat min');
 };
